062daba82f8642dc161c935a5f70ed4336e82ac1
dorkydhruv <thedhruvsharma@gmail.com>
2024-04-20 23:41:48 +0530

    web-socket added

--- lib/main.dart
+++ lib/main.dart
@@ -7,8 +7,6 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -21,7 +19,7 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       themeMode: ThemeMode.dark,
-      home: HomeView(),
+      home: const HomeView(),
     );
   }
 }
--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,51 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
 
-class HomeView extends StatefulWidget {
+class HomeView extends StatelessWidget {
   const HomeView({super.key});
-
-  @override
-  State<HomeView> createState() => _HomeViewState();
-}
-
-class _HomeViewState extends State<HomeView> {
-  final channel = WebSocketChannel.connect(Uri.parse("ws://localhost:8000/ws"));
-  TextEditingController _controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Form(
-                child: TextFormField(
-                  controller: _controller,
-                  decoration:
-                      const InputDecoration(labelText: "Send a message"),
-                ),
-              ),
-              StreamBuilder(
-                stream: channel.stream,
-                builder: (context, snapshot) {
-                  return Padding(
-                    padding: const EdgeInsets.all(20.0),
-                    child: Text(snapshot.hasData ? '${snapshot.data}' : ''),
-                  );
-                },
-              ),
-            ],
-          ),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            if (_controller.text.isNotEmpty) {
-              channel.sink.add(_controller.text);
-            }
-          },
-          child: const Icon(Icons.send),
-        ));
+      appBar: AppBar(
+        title: const Text('Let\'s Chat?'),
+      ),
+      body: const Center(
+        child: Text('Welcome to Chat App'),
+      ),
+    );
   }
 }
